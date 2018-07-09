58dd07cb98902142a7fb44633465774061c43d79
Barbara Vasic <barbara.vasic0108@gmail.com>
2018-07-09 14:25:00 +0200

    fiinish bit show

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import TopShowsPage from "./pages/TopShowsPage";
 import ShowInfoPage from "./pages/ShowInfoPage";
